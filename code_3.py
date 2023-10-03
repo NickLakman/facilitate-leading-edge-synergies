faed779680e9eaf8f3e1762a584dc873d9b18f5c
NickLakman <140800149+NickLakman@users.noreply.github.com>
2023-10-03 02:21:02 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,32 @@
+}
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+    });
+
+function main() {
+    }
+
+    return data;
+    for (let i = 0; i < 10; i++) {
+}
+
+main();
+    const data = [];
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();}
+    const data = [];
+    data.forEach(item => {
+
+        console.log("Random Number: " + item);
+main();
+function main() {
+    const data = generateRandomData();
+}
+    });
+    for (let i = 0; i < 10; i++) {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    return data;
+function generateRandomData() {
