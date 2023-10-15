c5286379418d81c6e8ffd55063cc68373ab20d06
NickLakman <140800149+NickLakman@users.noreply.github.com>
2023-10-15 16:39:25 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,30 +1,30 @@
-}
-function generateRandomData() {
-    return data;
-    const data = generateRandomData();
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {
-    const data = [];
 
-    });
-
-    data.forEach(item => {
-
-    }
-main();
-}
-function main() {
-        console.log("Random Number: " + item);
-def generate_random_data():
-    data = generate_random_data()
 import random
 
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
-
-    main()
+        print(f"Random Number: {item}")
     return data
-    for item in data:
 if __name__ == "__main__":
 
-        print(f"Random Number: {item}")
+    for item in data:
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+def generate_random_data():
+
+    main()    }
+function generateRandomData() {
+    });
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+}
+
+function main() {
+    for (let i = 0; i < 10; i++) {
+}
+        console.log("Random Number: " + item);
+    return data;
+    const data = [];
+
+    const data = generateRandomData();
+main();
