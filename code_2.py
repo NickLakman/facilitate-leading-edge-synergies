50346ff262d2319c76e4537cc69f5ff8bfe2006b
NickLakman <140800149+NickLakman@users.noreply.github.com>
2023-10-19 21:23:22 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,56 +1,39 @@
-    for item in data:
-
-
-def generate_random_data():
-    return data
-if __name__ == "__main__":
 def main():
     data = generate_random_data()
+
+        print(f"Random Number: {item}")
+
+
+    for item in data:
+
+    return data
+import random
+if __name__ == "__main__":
+def generate_random_data():
     data = [random.randint(1, 100) for _ in range(10)]
     main()
 
-        print(f"Random Number: {item}")
-import random
-if __name__ == "__main__":
-    for item in data:
-import random
-
-
-    data = generate_random_data()
-def generate_random_data():
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def main():
-    return data
     main()
-        print(f"Random Number: {item}")
-    data.forEach(item => {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    return data;
-function main() {
+if __name__ == "__main__":
+def main():
+
+
+def generate_random_data():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    for item in data:
+    data = generate_random_data()
+        print(f"Random Number: {item}")        for (int i = 0; i < 10; i++) {
+
+public class RandomDataGenerator {
+        for (int item : data) {
+        int[] data = new int[10];
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
 }
-    for (let i = 0; i < 10; i++) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
     }
-        console.log("Random Number: " + item);
-
-function generateRandomData() {
-main();
-    const data = generateRandomData();
-
-    const data = [];
-}
-    });def generate_random_data():
-        print(f"Random Number: {item}")
-def main():
-import random
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    main()
-
-    return data
-if __name__ == "__main__":
-    data = generate_random_data()
-    for item in data:
+        }
+        }
