57af9cd2d6310c687fe5804d91c720021d8fba35
NickLakman <140800149+NickLakman@users.noreply.github.com>
2023-10-25 03:08:46 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,40 +1,63 @@
-
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-    main()
-
 def generate_random_data():
-
-
-    return data
-    data = generate_random_data()
-def main():
-    for item in data:
-        print(f"Random Number: {item}")        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-def main():
-
-import random
-if __name__ == "__main__":
-    data = generate_random_data()
-
-    for item in data:
-
-    return data
-
-def generate_random_data():    for item in data:
-    main()
-
-
-import random
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-def generate_random_data():
-if __name__ == "__main__":
-
-def main():
         print(f"Random Number: {item}")
+    main()
+if __name__ == "__main__":
+
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
     return data
+def main():
+
+import random
+def generate_random_data():
+if __name__ == "__main__":
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+    data = generate_random_data()
+
+
+def main():
+    for item in data:
+    return data
+
+
+        print(f"Random Number: {item}")public class RandomDataGenerator {
+
+        for (int i = 0; i < 10; i++) {
+}
+        int[] data = new int[10];
+
+        for (int item : data) {
+    }
+            System.out.println("Random Number: " + item);
+        }
+        }
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        for (int item : data) {
+        }
+    public static void main(String[] args) {
+        }
+public class RandomDataGenerator {
+}
+
+        }
+    }
+
+public class RandomDataGenerator {
+        }
+        for (int i = 0; i < 10; i++) {
+
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        int[] data = new int[10];
+        for (int item : data) {
